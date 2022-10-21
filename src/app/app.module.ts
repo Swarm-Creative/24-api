11dc47eeb5c7e31a8b64905a2703877ec5260ab2
Simon Norman <zhymon.norman@gmail.com>
2022-10-21 17:58:22 -0400

    fix: use Railway env, add @nest/config support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,24 @@ import { LeaderboardModule } from 'src/leaderboard/leaderboard.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MONO_DB_CONNECTION_STRING } from 'src/constants';
 import { UserModule } from 'src/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:bPhaTx8VOLImFHrpLtHF@containers-us-west-105.railway.app:7901'),    
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: `mongodb://${config.get<string>('MONGOUSER')}:${config.get<string>('MONGOPASSWORD')}@${config.get<string>('MONGOHOST')}:${config.get<string>('MONGOPORT')}`        
+      })
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: 'schema.gql',
-  }), LeaderboardModule, UserModule
+  }), LeaderboardModule, UserModule,    
 ],
   controllers: [AppController],
   providers: [AppService],
